70d56519269f3b9b64b6d6436defd9d88715aeff
Shubhadeep <shubhadeeproychowdhury@gmail.com>
2015-01-31 17:53:14 +0530

    #WIP commit. testing

---
+++ drivers/pb/pb.go
@@ -0,0 +1,64 @@
+package pb
+
+import (
+	"fmt"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
+	"github.com/docker/machine/drivers"
+)
+
+const (
+	dockerConfigDir = "/etc/docker"
+)
+
+type Driver struct {
+	Userid		string
+	Password	int
+}
+
+func init() {
+	drivers.Register("pb", &drivers.RegisteredDriver{
+		New:            NewDriver,
+		GetCreateFlags: GetCreateFlags,
+	})
+}
+
+func GetCreateFlags() []cli.Flag {
+	return []cli.Flag{
+		cli.StringFlag{
+			EnvVar: "PB_USER",
+			Name:   "pb-user",
+			Usage:  "Profiitbricks user id",
+		},
+		cli.StringFlag{
+			EnvVar: "PB_PASSWD",
+			Name:   "pb-password",
+			Usage:  "Profitbricks password",
+		},
+	}
+}
+
+func NewDriver(machineName string, storePath string, caCert string, privateKey string) (drivers.Driver, error) {
+	return &Driver{MachineName: machineName, storePath: storePath, CaCertPath: caCert, PrivateKeyPath: privateKey}, nil
+}
+
+func (d *Driver) DriverName() string {
+	return "pb"
+}
+
+func (d *Driver) SetConfigFromFlags(flags drivers.DriverOptions) error {
+	d.Userid = flags.String("pb-user")
+	d.Password = flags.String("pb-password")
+	return nil
+}
+
+func (d *Driver) PreCreateCheck() error {
+	return nil
+}
+
+func (d *Driver) Create() error {
+	log.Infof("Creating SSH key...")
+	return nil
+}
+
