e625c1aec28f40ea3556a4b4f038f608bba69975
Shubhadeep <shubhadeeproychowdhury@gmail.com>
2015-02-15 23:10:01 +0530

    #WIP Commit

--- drivers/pb/pb.go
+++ drivers/pb/pb.go
@@ -271,7 +271,7 @@ func (d *Driver) Create() error {
 	if err != nil {
 		return err
 	}
-
+	log.Infof("UserID ---- %s", d.Userid)
 	//Get vdc ID from name
 	log.Infof("%s", d.VDCName)
 	log.Debugf("%+v", key)	
